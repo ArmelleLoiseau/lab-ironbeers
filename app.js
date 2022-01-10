5660efb01e117cf98f8bb7bc0ebbbe99774f2635
Armelle Loiseau <armelle.loiseau@gmail.com>
2022-01-10 23:48:36 +0100

    done but still needs  styling (random-beer and one-beer) + bug display objects in one-beer to fix

--- app.js
+++ app.js
@@ -13,13 +13,49 @@ app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Register the location for handlebars partials here:
-
+hbs.registerPartials(__dirname + '/views/partials');
 // ...
 
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
   res.render('index');
+});
+
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(data => {
+      res.render('beers.hbs', {
+        beers: data
+      });
+      // res.send(JSON.stringify(data));
+      // console.log(`the beers are : ${readableData}`);
+    })
+    .catch(error => console.error(error));
+});
+
+app.get('/random-beer', (req, res) => {
+  punkAPI
+    .getRandom()
+    .then(beer => {
+      res.render('random-beer.hbs', {
+        beer
+      });
+    })
+    .catch(e => console.error(e));
+});
+
+app.get('/beers/:id', (req, res) => {
+  const id = req.params.id;
+  punkAPI
+    .getBeer(id)
+    .then(data => {
+      res.render('one-beer.hbs', {
+        beer: data
+      });
+    })
+    .catch(e => console.error(e));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
